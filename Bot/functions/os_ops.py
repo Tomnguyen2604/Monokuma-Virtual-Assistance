8cdc21f4bb3cb9a9dd35024e2632b87a312abccd
Tomnguyen2604 <63090801+Tomnguyen2604@users.noreply.github.com>
2022-04-11 19:18:57 -0600

    first commit

---
+++ Bot/functions/os_ops.py
@@ -0,0 +1,26 @@
+import os
+import subprocess as sp
+
+paths = {
+    'notepad': "C:\\Windows\\notepad.exe",
+    'discord': "C:\\Users\\tomng\\AppData\\Local\\Discord\\app-1.0.9004\\Discord.exe",
+    'calculator': "C:\\Windows\\System32\\calc.exe"
+}
+def open_notepad():
+    os.startfile(paths['notepad'])
+
+
+def open_discord():
+    os.startfile(paths['discord'])
+
+
+def open_cmd():
+    os.system('start cmd')
+
+
+def open_camera():
+    sp.run('start microsoft.windows.camera:', shell=True)
+
+
+def open_calculator():
+    sp.Popen(paths['calculator'])
---
+++ Bot/utils.py
@@ -0,0 +1,4 @@
+opening_text = [
+    'Give me a moment, sir.',
+    'Working on it!',
+    'Just one secon
